ddf88bc5b4d046b1a9909715f4103e6a92828db0
anne-marziale <annemarziale@hotmail.com>
2022-03-01 18:03:09 +0100

    first init

---
+++ app.js
@@ -0,0 +1,19 @@
+const titleSpans = document.querySelectorAll('h1 span');
+const btns = document.querySelectorAll('.btn-first');
+const medias = document.querySelectorAll('.bulle');
+const l1 = document.querySelector('.l1');
+const l2 = document.querySelector('.l2');
+
+window.addEventListener('load', () => {
+
+    const TL = gsap.timeline({paused :true});
+
+    TL
+    .staggerFrom(titleSpans, 1, {top: -50, opacity: 0, ease: "power2.out"}, 0.3)
+    .staggerFrom(btns, 1, {opacity: 0, ease: "power2.out"}, 0.3, '-=1')
+    .from(l1, 1, {width: 0, ease: "power2.out"}, '-=2')
+    .from(l2, 1, {width: 0, ease: "power2.out"}, '-=2')
+    .staggerFrom(medias, 1, {right: -200, ease: "power2.out"}, 0.1, '-=1');
+
+    TL.play();
+})
